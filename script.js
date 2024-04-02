b57c60622153735566d6fd77ee2fc05fbc4d45cf
Krakaw <41575888+Krakaw@users.noreply.github.com>
2024-04-02 09:57:10 -0700

    Clean up editor and add hiding

--- script.js
+++ script.js
@@ -204,6 +204,41 @@ function update() {
     requestAnimationFrame(update)
 }
 
-// Initialization
-addTicks();
-requestAnimationFrame(update);
+
+function initialize() {
+    const container = document.getElementById("jsoneditor")
+    const toggleEditorButton = document.getElementById("toggle-editor");
+    toggleEditorButton.addEventListener('click', function() {
+        container.classList.toggle('visible');
+    });
+    const options = {
+        modes: ['code', 'tree'],
+        colorPicker: true,
+        templates: [
+            {
+                text: 'Timezone',
+                title: 'Insert a timezone',
+                value: {
+                    timezone: 'UTC',
+                    label: 'Me',
+                    color: '#ff0000',
+                    imageUrl: '',
+                }
+    
+            },
+    
+        ],
+        onChange: () => {
+            localStorage.setItem('timezones', JSON.stringify(editor.get()));
+            update();
+        },
+    }
+    const editor = new JSONEditor(container, options);
+    editor.set(JSON.parse(localStorage.getItem('timezones') || '[]'))
+    
+    // Initialization
+    addTicks();
+    requestAnimationFrame(update);
+}
+
+initialize();
