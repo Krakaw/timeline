f2a27bf983fb2f2809c1a8a21385427e6a633bec
Krakaw <41575888+Krakaw@users.noreply.github.com>
2024-10-18 08:26:24 -0700

    Fix the time formatting

--- src/app/utils/parseParams.ts
+++ src/app/utils/parseParams.ts
@@ -9,7 +9,9 @@ export function parseInputParamsArray(arr: string[]) {
         if (item.match(/\d{1,2}:\d{1,2}/)) {
             if (!from_time) {
                 // Populate from_time if item contains a colon (assuming it's a time value)
-                from_time = item;
+                // Make sure the time always has a leading zero
+                const [hours, minutes] = item.split(':');
+                from_time = `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}`;
             }
         } else if (!from_zone) {
             // If from_zone is not yet populated, use the first non-time value
